09e4d19790e27e8422a613c731a1f31b618db6b2
nouveua <mahbub022@gmail.com>
2023-01-31 23:21:57 +0530

    nightwatch demo

---
+++ tests/dolphinHomePage.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "Lets connect over conference !": browser => {
+        browser.url("https://nouveau-labs.dolphinvc.com")    
+        browser.pause(5 * 1000)        
+    }
+}
