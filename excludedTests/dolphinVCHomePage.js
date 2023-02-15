52f53a039fd219f93c38f33aefe081e6b3ca5277
michael0022 <mahbub022@gmail.com>
2023-02-15 10:36:47 +0530

    multipleParticipants

--- excludedTests/dolphinVCHomePage.js
+++ excludedTests/dolphinVCHomePage.js
@@ -7,19 +7,21 @@ module.exports = {
         //browser.pause(15 * 1000)
         const nameXpath = '//*[@id="root"]/div[4]/div/div/div[2]/div[3]/div[1]/div[2]/div/input'
         browser.click('//*[@id="root"]/div[4]/div/div/div[2]/div[2]/div[3]/div/div/div[2]')
-        browser.setValue(nameXpath, "Aditya")
-        
-
+        browser.setValue(nameXpath, "Mahbub Ansari")
+        browser.pause(4 * 1000)
+        browser.saveScreenshot('./reports/name.png')
         browser.click('//*[@id="joinNow"]/span')
         
-
+        browser.pause(14 * 1000)
         browser.click('//*[@id="root"]/div[3]/div/div/div[2]/span')
-
+        browser.pause(14 * 1000)
         browser.click('//*[@id="root"]/div[3]/div[2]/div/div[3]/div/div/div[2]')
-
+       // browser.pause(14 * 1000)
+        browser.saveScreenshot('./reports/selection.png')
         browser.click('//*[@id="root"]/div[3]/div[2]/div/div[4]/div/div/button/span')
-        browser.pause(5*1000)
+        browser.pause(5 * 1000)
+        browser.saveScreenshot('./reports/conference.png')
+        browser.pause()
         browser.end()
-
     }
 }
--- excludedTests/dolphinVCHomePage2.js
+++ excludedTests/dolphinVCHomePage2.js
@@ -7,18 +7,18 @@ module.exports = {
         //browser.pause(15 * 1000)
         const nameXpath = '//*[@id="root"]/div[4]/div/div/div[2]/div[3]/div[1]/div[2]/div/input'
         browser.click('//*[@id="root"]/div[4]/div/div/div[2]/div[2]/div[3]/div/div/div[2]')
-        browser.setValue(nameXpath, "Ravindra")
+        browser.setValue(nameXpath, "Aditya")
         
-
+        browser.pause(14 * 1000)
         browser.click('//*[@id="joinNow"]/span')
         
-
+        browser.pause(14 * 1000)
         browser.click('//*[@id="root"]/div[3]/div/div/div[2]/span')
-
+        //browser.pause(14 * 1000)
         browser.click('//*[@id="root"]/div[3]/div[2]/div/div[3]/div/div/div[2]')
 
         browser.click('//*[@id="root"]/div[3]/div[2]/div/div[4]/div/div/button/span')
-        browser.pause(5*1000)
+        browser.pause()
         browser.end()
 
     }
